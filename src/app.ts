577d567ca7c2d20ce9353f2d5934902b6d6241e5
Nizar K <nizar.dev01@gmail.com>
2021-01-26 17:22:43 +0530

    _createCanvas method abstracted

--- src/app.ts
+++ src/app.ts
@@ -24,25 +24,16 @@ class App {
     private _scene : Scene;
     private _canvas : HTMLCanvasElement;
     private _engine : Engine;
+    
+    private _gameScene: Scene | undefined;
+    private _cutScene: Scene | undefined;
 
-    private _state: number = 0;
+    private _state: Number = 0;
 
     constructor(){
-        this._canvas = document.createElement('canvas');
-        this._canvas.style.display = "block";
-        this._canvas.style.width = "100%";
-        this._canvas.style.height = "100vh";
-        this._canvas.id = "gameCanvas";
-        document.body.appendChild(this._canvas);
-
+        this._canvas = this._createCanvas();
         this._engine = new Engine(this._canvas,true);
         this._scene = new Scene(this._engine);
-
-        const camera : ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), this._scene);
-        camera.attachControl(this._canvas,true);
-
-        const light1 : HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), this._scene);
-        const sphere : Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, this._scene);
 
         window.addEventListener("keydown", e =>{
             // Shift+Ctrl+Alt+I
@@ -55,18 +46,59 @@ class App {
             }
         });
 
-        window.addEventListener("resize",()=>{
-            this._engine.resize();
+        this._main();
+        
+    }
+
+    private _createCanvas(): HTMLCanvasElement {
+        document.documentElement.style["overflow"] = "hidden";
+        document.documentElement.style.overflow = "hidden";
+        document.documentElement.style.width = "100%";
+        document.documentElement.style.height = "100%";
+        document.documentElement.style.margin = "0";
+        document.documentElement.style.padding = "0";
+        document.body.style.overflow = "hidden";
+        document.body.style.width = "100%";
+        document.body.style.height = "100%";
+        document.body.style.margin = "0";
+        document.body.style.padding = "0";
+
+        const canvas = document.createElement('canvas');
+        canvas.style.display = "block";
+        canvas.style.width = "100%";
+        canvas.style.height = "100%";
+        canvas.id = "gameCanvas";
+
+        document.body.appendChild(canvas);
+        return canvas;
+    }
+
+    private async _main(): Promise<void>{
+        await this._goToStart();
+        
+        this._engine.runRenderLoop(()=>{
+            switch (this._state){
+                case State.START:
+                    this._scene.render();
+                    break;
+                case State.CUTSCENE:
+                    this._scene.render();
+                    break;
+                case State.GAME:
+                    this._scene.render();
+                    break;
+                case State.LOSE:
+                    this._scene.render();
+                    break;
+                default: break;
+            }
         });
 
-        this._engine.runRenderLoop(()=>{
-            this._scene.render();
-        })
+        window.addEventListener("resize",()=> this._engine.resize() );
     }
 
     private async _goToStart() {
         this._engine.displayLoadingUI();
-
         this._scene.detachControl();
 
         const scene = new Scene(this._engine);
@@ -104,8 +136,107 @@ class App {
         })
     }
 
-    private async _goToCutScene(){
-        
+    private async _goToCutScene(): Promise<void> {
+        this._engine.displayLoadingUI();
+        this._scene.detachControl();
+
+        // create cutScene
+        this._cutScene = new Scene(this._engine);
+        const camera = new FreeCamera("camera1", new Vector3(0, 0, 0), this._cutScene);
+        camera.setTarget(Vector3.Zero());
+        this._cutScene.clearColor = new Color4(0, 0, 0, 1);
+
+
+        // Create GUI
+        const testUI = AdvancedDynamicTexture.CreateFullscreenUI("UI");
+        // Next Button ( for testing purposes )
+        const next = Button.CreateSimpleButton("next", "START PLAYING");
+        next.color = "white";
+        next.thickness = 0;
+        next.verticalAlignment = Control.VERTICAL_ALIGNMENT_BOTTOM;
+        next.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_RIGHT;
+        next.width = "200px"
+        next.height = "64px";
+        next.top = "-3%";
+        next.left = "-12%";
+        testUI.addControl(next);
+
+        next.onPointerDownObservable.add(()=>{
+            this._goToGame();
+        });
+
+        await this._cutScene.whenReadyAsync();
+        this._engine.hideLoadingUI();
+        this._scene.dispose();
+        this._state = State.CUTSCENE;
+        this._scene = this._cutScene;
+
+        let finishedLoading = false;
+        await this._setUpGame().then( res => {
+            finishedLoading = true;
+        });
+
+    }
+
+    private async _setUpGame(){
+        const scene = new Scene(this._engine);
+        this._gameScene = scene;
+
+        //...load assets
+    }
+
+    private async _goToGame(){
+        // Setting up the scene
+        this._scene.detachControl();
+        const scene = this._gameScene;
+        if(scene){
+            scene.clearColor = new Color4(0.01568627450980392, 0.01568627450980392, 0.20392156862745098);
+            const camera = new ArcRotateCamera(
+                "Camera", // name
+                Math.PI /2, // camera rotation along the longitudinal axis.
+                Math.PI /2, // camera rotation along the latitudinal axis
+                2, // radius
+                Vector3.Zero(), // target
+                scene // scene
+            );
+            camera.setTarget(Vector3.Zero());
+
+            const playerUI = AdvancedDynamicTexture.CreateFullscreenUI("UI");
+
+            // disable all inputs from while the game is loading
+            scene.detachControl();
+
+            const loseBtn = Button.CreateSimpleButton("lose", "LOSE");
+            loseBtn.width = 0.2
+            loseBtn.height = "40px";
+            loseBtn.color = "white";
+            loseBtn.top = "-14px";
+            loseBtn.thickness = 0;
+            loseBtn.verticalAlignment = Control.VERTICAL_ALIGNMENT_BOTTOM;
+            playerUI.addControl(loseBtn);
+
+            loseBtn.onPointerDownObservable.add(()=>{
+                this._goToLose();
+                scene.detachControl();
+            });
+
+            // LIGHTS
+            const light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+            const spehere: Mesh = MeshBuilder.CreateSphere(
+                "sphere",
+                {
+                    diameter: 1
+                },
+                scene
+            );
+
+            //get rid of start scene, switch to gamescene and change states
+            this._scene.dispose();
+            this._state = State.GAME;
+            this._scene = scene;
+            this._engine.hideLoadingUI();
+            this._scene.attachControl();
+        }
     }
 
     private async _goToLose() : Promise<void> {
